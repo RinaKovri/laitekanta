d2a43de167fd8608b270601686e491f88fbfb80c
abhi1988sharma <abhi1988.sharma@gmail.com>
2023-05-08 12:13:32 +0300

    Updated

--- Client/src/components/AddingForm.jsx
+++ Client/src/components/AddingForm.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import Axios from 'axios';
 
-
 const AddingForm = () => {
 
     const [email, setEmail] = useState("");
@@ -11,6 +10,7 @@ const AddingForm = () => {
     const [returningDate, setReturningDate] = useState("");
     const [deviceList, setDeviceList] = useState([]);
     const [newSerial, setNewSerial] = useState("");
+    const [searchTerm, setSearchTerm] = useState("");
 
     useEffect (() => {
         Axios.get("http://localhost:3001/api/get").then((response)=>{
@@ -28,17 +28,22 @@ const AddingForm = () => {
         });
 
         setDeviceList([
-        ...deviceList, { 
-            email: email, 
-            serialNumber: serialNumber,
-            issuingDate: issuingDate, 
-            returningDate: returningDate,},
-            ])
+            ...deviceList, { 
+                email: email, 
+                serialNumber: serialNumber,
+                issuingDate: issuingDate, 
+                returningDate: returningDate,
+            },
+        ])
         
     };
 
     const deleteDevice = (item) => {
-        Axios.delete(`http://localhost:3001/api/delete/${item}`)
+        Axios.delete(`http://localhost:3001/api/delete/${item}`).then(() => {
+            setDeviceList(deviceList.filter((val) => {
+                return val.email !== item;
+            }))
+        })
     }
     
 
@@ -46,45 +51,69 @@ const AddingForm = () => {
         Axios.put("http://localhost:3001/api/update", {
             email: item, 
             serialNumber: newSerial,
-        
         })
         setNewSerial("")
     }
     
+    const handleSearchTermChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const searchDevice = (device) => {
+        if (
+            device.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            device.serialNumber.toLowerCase().includes(searchTerm.toLowerCase())
+        ) {
+            return true;
+        }
+        return false;
+    };
 
     return (
-<div className='form'>
-      <h1>Laitekanta Lisaa</h1>
-      <div className='form'>
-      <label>Email:</label>
-      <input type= "text" name="email" onChange={(e) => {setEmail(e.target.value)}}/>
-      <label>Serial Number</label>
-      <input type= "text" name="serialNumber" onChange={(e) => {setSerialNumber(e.target.value)}}/>
-      <label>Issuing Date</label>
-      <input type= "date" name="issuingDate" onChange={(e) => {setIssuingDate(e.target.value)}}/>
-      <label>Returning Date</label>
-      <input type= "date" name="issuingDate" onChange={(e) => {setReturningDate(e.target.value)}}/>
-      <button onClick={addDevice}>Lisää</button>
+        <div className='form'>
+        <h1>Laitekanta Lisaa</h1>
+        <div className='form'>
+          <label>Email:</label>
+          <input type="text" name="email" onChange={(e) => { setEmail(e.target.value) }} />
+          <label>Serial Number</label>
+          <input type="text" name="serialNumber" onChange={(e) => { setSerialNumber(e.target.value) }} />
+          <label>Issuing Date</label>
+          <input type="date" name="issuingDate" onChange={(e) => { setIssuingDate(e.target.value) }} />
+          <label>Returning Date</label>
+          <input type="date" name="returningDate" onChange={(e) => { setReturningDate(e.target.value) }} />
+          <button onClick={addDevice}>Lisää</button>
+          <br />
+          <br />
+          <label>Search:</label>
+          <input type="text" onChange={handleSearchTermChange} />
+    
+          {searchTerm !== "" && deviceList.filter(searchDevice).map((val) => {
+            
+    const issuingDateFormatted = val.issuingDate ? new Date(val.issuingDate).toLocaleDateString('fi-FI') : '';
+    const returningDateFormatted = val.returningDate ? new Date(val.returningDate).toLocaleDateString('fi-FI') : '';
+    return (
+        <div className='card'>
+            <h1>EMAIL:{val.email}</h1>
+            <h3>SERIAL NUMBER: {val.serialNumber}</h3>
+            <h3>ISSUING DATE: {issuingDateFormatted}</h3>
+            <h3>RETURNING DATE: {returningDateFormatted}</h3>
 
-   {deviceList.map((val)=>{
-        return <div className='card'>
-        <h1>EMAIL:{val.email}</h1>
-        <h3>SERIAL NUMBER: {val.serialNumber}</h3>
-        <h3>ISSUING DATE: {val.issuingDate}</h3>
-        <h3>RETURNING DATE: {val.returningDate}</h3>
+            <button onClick={() => {
+                if (window.confirm(`Are you sure you want to delete the device with email ${val.email}?`)) {
+                    deleteDevice(val.email)
+                }
+            }}>DELETE</button>
 
-        <button onClick={() => {
-    if (window.confirm(`Are you sure you want to delete the device with email ${val.email}?`)) {
-        deleteDevice(val.email)
-    }
-}}>DELETE</button>
-
-        <input type="text" placeholder='update serial no' id="updateInput" onChange={(e) => {setNewSerial(e.target.value)}}/>
-        <button onClick={() => {updateDevice(val.email)}}>UPDATE</button>
+            <input type="text" placeholder='update serial no' id="updateInput" onChange={(e) => { setNewSerial(e.target.value) }} />
+            <button onClick={() => { updateDevice(val.email) }}>UPDATE</button>
         </div>
-      })}
+    )
+})}
+
+        </div>
       </div>
-      </div>
-)}
+    )
+    
+}
 
 export default AddingForm
